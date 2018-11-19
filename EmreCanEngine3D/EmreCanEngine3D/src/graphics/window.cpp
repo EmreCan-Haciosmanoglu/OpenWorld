a4e4fa4fdb1c9017d598ed71438b44baccd511af
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-20 01:21:56 +0300

    Window cpp file

--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.cpp
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.cpp
@@ -1,1 +1,16 @@
 #include "window.h"
+namespace EmreCan3D
+{
+	namespace graphics
+	{
+		Window::Window(const char * name, int width, int height)
+		{
+		}
+		Window::~Window()
+		{
+		}
+		void Window::update() const
+		{
+		}
+	}
+}
