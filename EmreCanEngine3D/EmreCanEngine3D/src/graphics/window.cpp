f5edca7b712451e30d46f57f99bef77d88333843
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-20 01:39:35 +0300

    implementation of init and closed func

--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.cpp
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.cpp
@@ -4,20 +4,34 @@ namespace EmreCan3D
 {
 	namespace graphics
 	{
-		Window::Window(const char * name, int width, int height)
+		Window::Window(const char * title, int width, int height)
 		{
-			m_Name = name;
+			m_Title = title;
 			m_Width = width;
 			m_Height = height;
+			init();
 		}
 		Window::~Window()
 		{
+		}
+		bool Window::closed() const
+		{
+			return glfwWindowShouldClose(m_Window);
 		}
 		void Window::update() const
 		{
 		}
 		void Window::init()
 		{
+			m_Window = glfwCreateWindow(m_Width, m_Height,m_Title, NULL,NULL);
+			if (!m_Window)
+			{
+				glfwTerminate();
+				std::cout << "Failed to create GLFW window!" << std::endl;
+				return;
+			}
+			glfwMakeContextCurrent(m_Window);
+
 		}
 	}
 }
--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.h
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <GLFW/glfw3.h>
+#include<iostream>
 
 namespace EmreCan3D
 {
@@ -8,12 +9,14 @@ namespace EmreCan3D
 		class Window
 		{
 		private:
-			const char *m_Name;
+			const char *m_Title;
 			int m_Width, m_Height;
 			GLFWwindow *m_Window;
+			bool m_Closed;
 		public:
-			Window(const char *name, int width, int height);
+			Window(const char *title, int width, int height);
 			~Window();
+			bool closed() const;
 			void update() const;
 		private:
 			void init();
