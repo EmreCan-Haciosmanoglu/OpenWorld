2f5ab6b26adacab7583b8c93f1dc9fb6494e42f7
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-20 01:19:55 +0300

    Window header file

--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.h
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/window.h
@@ -1,1 +1,17 @@
 #pragma once
+namespace EmreCan3D
+{
+	namespace graphics
+	{
+		class Window
+		{
+		private:
+			const char *name;
+			int width, height;
+		public:
+			Window(const char *name, int width, int height);
+			~Window();
+			void update() const;
+		};
+	}
+}
