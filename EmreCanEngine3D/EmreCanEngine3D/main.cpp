76d0dbafb624898f1d8ee6f0cfccf599aa23979e
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-20 19:49:26 +0300

    creating first window

--- EmreCanEngine3D/EmreCanEngine3D/main.cpp
+++ EmreCanEngine3D/EmreCanEngine3D/main.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 #include<GLFW/glfw3.h>
+#include "src/graphics/window.h"
 
 int main()
 {
+	using namespace EmreCan3D;
+	using namespace graphics;
 
+	Window window("3D Game Engine", 800, 600);
+
+	while (!window.closed())
+	{
+		window.update();
+	}
 	system("PAUSE");
 	return 0;
 }
