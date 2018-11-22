1cdd5c7bd0654540d7e6109be323537daa90abbb
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-22 19:43:00 +0300

    defining funcs

--- EmreCanEngine3D/EmreCanEngine3D/src/maths/vec2.cpp
+++ EmreCanEngine3D/EmreCanEngine3D/src/maths/vec2.cpp
@@ -7,25 +7,37 @@ namespace EmreCan3D
 	{
 		vec2::vec2()
 		{
+			x = 0.0f;
+			y = 0.0f;
 		}
 		vec2::vec2(const float & x, const float & y)
 		{
+			this->x = x;
+			this->y = y;
 		}
 		vec2 & vec2::add(const vec2 & other)
 		{
-			// TODO: döndürme ifadesini buraya ekleyin
+			x += other.x;
+			y += other.y;
+			return (*this);
 		}
 		vec2 & vec2::subtract(const vec2 & other)
 		{
-			// TODO: döndürme ifadesini buraya ekleyin
+			x -= other.x;
+			y -= other.y;
+			return (*this);
 		}
 		vec2 & vec2::multiply(const vec2 & other)
 		{
-			// TODO: döndürme ifadesini buraya ekleyin
+			x *= other.x;
+			y *= other.y;
+			return (*this);
 		}
 		vec2 & vec2::devide(const vec2 & other)
 		{
-			// TODO: döndürme ifadesini buraya ekleyin
+			x /= other.x;
+			y /= other.y;
+			return (*this);
 		}
 	}
 }
