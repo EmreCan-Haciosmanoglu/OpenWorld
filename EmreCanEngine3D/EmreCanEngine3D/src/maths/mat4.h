020d8598b9e3dc700c45307638fb5d979fe26596
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-26 23:11:48 +0300

    A little trick

--- EmreCanEngine3D/EmreCanEngine3D/src/maths/mat4.h
+++ EmreCanEngine3D/EmreCanEngine3D/src/maths/mat4.h
@@ -9,7 +9,11 @@ namespace EmreCan3D
 	{
 		struct mat4
 		{
-			float elements[4 * 4];
+			union
+			{
+				float elements[4 * 4];
+				vec4 columns[4];
+			};
 			mat4();
 			mat4(float diagoal);
 
