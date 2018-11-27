b01cfec06c3dd3accd928396212157eceda738c4
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-11-27 23:08:57 +0300

    Shader load func

--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/shader.cpp
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/shader.cpp
@@ -1,17 +1,80 @@
 #include "shader.h"
 
-EmreCan3D::graphics::Shader::Shader(const char * vertPath, const char * fragPath)
+namespace EmreCan3D
 {
-}
+	namespace graphics
+	{
+		Shader::Shader(const char * vertPath, const char * fragPath)
+			:m_VertPath(vertPath)
+			,m_FragPath(fragPath)
+		{
+			m_ShaderID = load();
+		}
 
-EmreCan3D::graphics::Shader::~Shader()
-{
-}
+		Shader::~Shader()
+		{
+		}
 
-void EmreCan3D::graphics::Shader::enable() const
-{
-}
+		void Shader::enable() const
+		{
+		}
 
-void EmreCan3D::graphics::Shader::disable() const
-{
+		void Shader::disable() const
+		{
+		}
+		GLuint Shader::load()
+		{
+			GLuint program = glCreateProgram();
+			GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
+			GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+			const char *vertSource = read_file(m_VertPath).c_str();
+			const char *fragSource = read_file(m_FragPath).c_str();
+
+			glShaderSource(vertex, 1, &vertSource, NULL);
+			glCompileShader(vertex);
+
+			GLint result;
+			glGetShaderiv(vertex, GL_COMPILE_STATUS, &result);
+			if (result == GL_FALSE)
+			{
+				GLint lenght;
+				glGetShaderiv(vertex, GL_INFO_LOG_LENGTH, &lenght);
+				std::vector<char> error(lenght);
+				glGetShaderInfoLog(vertex, lenght, &lenght, &error[0]);
+				
+				std::cout << "Failed to compile vertex shader!" << std::endl << &error[0] << std::endl;
+				glDeleteShader(vertex);
+				return 0;
+			}
+
+			glShaderSource(fragment, 1, &fragSource, NULL);
+			glCompileShader(fragment);
+
+			GLint result;
+			glGetShaderiv(fragment, GL_COMPILE_STATUS, &result);
+			if (result == GL_FALSE)
+			{
+				GLint lenght;
+				glGetShaderiv(fragment, GL_INFO_LOG_LENGTH, &lenght);
+				std::vector<char> error(lenght);
+				glGetShaderInfoLog(fragment, lenght, &lenght, &error[0]);
+
+				std::cout << "Failed to compile fragment shader!" << std::endl << &error[0] << std::endl;
+				glDeleteShader(fragment);
+				return 0;
+			}
+
+			glAttachShader(program, vertex);
+			glAttachShader(program, fragment);
+
+			glLinkProgram(program);
+			glValidateProgram(program);
+
+			glDeleteShader(vertex);
+			glDeleteShader(fragment);
+
+			return program;
+		}
+	}
 }
--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/shader.h
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/shader.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
 #include <GL/glew.h>
 #include "../utils/fileutils.h"
 
@@ -10,13 +12,16 @@ namespace EmreCan3D
 		class Shader
 		{
 		private:
-			GLuint shader;
+			GLuint m_ShaderID;
+			const char *m_VertPath, *m_FragPath; //For Debug
 		public:
 			Shader(const char * vertPath, const char *fragPath);
 			~Shader();
 
 			void enable() const;
 			void disable() const;
+		private:
+			GLuint load();
 		};
 	}
 }
