45f13e25dd901738897e8b396b615a9854e0009a
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-12-06 02:03:09 +0300

    fast renderer

---
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/batchrenderer2d.cpp
@@ -0,0 +1,56 @@
+#include "batchrenderer2d.h"
+namespace EmreCan3D
+{
+	namespace graphics
+	{
+		BatchRenderer2D::BatchRenderer2D()
+		{
+			init();
+		}
+		BatchRenderer2D::~BatchRenderer2D()
+		{
+			delete m_IBO;
+			glDeleteBuffers(1, &m_VBO);
+		}
+		void BatchRenderer2D::submit(const Renderable2D * renderable)
+		{
+
+		}
+		void BatchRenderer2D::flush()
+		{
+		}
+		void BatchRenderer2D::init()
+		{
+			glGenVertexArrays(1, &m_VAO);
+			glGenBuffers(1, &m_VBO);
+
+			glBindVertexArray(m_VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+			glBufferData(GL_ARRAY_BUFFER, RENDERER_BUFFER_SIZE, NULL, GL_DYNAMIC_DRAW);
+			glEnableVertexAttribArray(SHADER_VERTEX_INDEX);
+			glEnableVertexAttribArray(SHADER_COLOR_INDEX);
+			glVertexAttribPointer(SHADER_VERTEX_INDEX, 3, GL_FLOAT, GL_FALSE, RENDERER_VERTEX_SIZE, (const GLvoid*)(0));
+			glVertexAttribPointer(SHADER_COLOR_INDEX,  4, GL_FLOAT, GL_FALSE, RENDERER_VERTEX_SIZE, (const GLvoid*)(3 * GL_FLOAT));
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+			GLushort indices[RENDERER_INDICES_SIZE];
+
+			int offset = 0;
+			for (int i = 0; i < RENDERER_INDICES_SIZE; i += 6)
+			{
+				indices[  i  ] = offset + 0;
+				indices[i + 1] = offset + 1;
+				indices[i + 2] = offset + 2;
+			
+				indices[i + 3] = offset + 2;
+				indices[i + 4] = offset + 3;
+				indices[i + 5] = offset + 0;
+
+				offset += 4;
+			}
+
+			m_IBO = new IndexBuffer(indices, RENDERER_INDICES_SIZE);
+			glBindVertexArray(0);
+		}
+	}
+}
---
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/batchrenderer2d.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "renderer2d.h"
+#include "buffers/indexbuffer.h"
+namespace EmreCan3D
+{
+	namespace graphics
+	{
+
+#define RENDERER_MAX_SPRITES	10000
+#define RENDERER_VERTEX_SIZE	sizeof(VertexData)
+#define RENDERER_SPRITE_SIZE	RENDERER_VERTEX_SIZE * 4
+#define RENDERER_BUFFER_SIZE	RENDERER_SPRITE_SIZE * RENDERER_MAX_SPRITES
+#define RENDERER_INDICES_SIZE	RENDERER_MAX_SPRITES * 6
+
+#define SHADER_VERTEX_INDEX		0
+#define SHADER_COLOR_INDEX		1
+		class BatchRenderer2D : public Renderer2D
+		{
+		private:
+			GLuint m_VBO;
+			GLuint m_VAO;
+			IndexBuffer* m_IBO;
+			GLsizei m_IndexCount;
+		public:
+			BatchRenderer2D();
+			~BatchRenderer2D();
+			void submit(const Renderable2D* renderable) override;
+			void flush() override;
+		private:
+			void init();
+		};
+	}
+}
