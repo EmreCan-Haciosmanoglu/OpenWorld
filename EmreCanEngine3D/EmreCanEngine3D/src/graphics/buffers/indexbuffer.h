52b3ddcd2221b87a60aa7c0affe21506363e0c17
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2018-12-13 01:48:34 +0300

    Temp changes

--- EmreCanEngine3D/EmreCanEngine3D/src/graphics/buffers/indexbuffer.h
+++ EmreCanEngine3D/EmreCanEngine3D/src/graphics/buffers/indexbuffer.h
@@ -12,6 +12,7 @@ namespace EmreCan3D
 			GLuint m_Count;
 		public:
 			IndexBuffer(GLushort *data, GLsizei count);
+			IndexBuffer(GLuint *data, GLsizei count);
 			~IndexBuffer();
 			void bind() const;
 			void unbind() const;
