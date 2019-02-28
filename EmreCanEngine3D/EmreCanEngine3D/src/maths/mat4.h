2220c3b6afa39c74a314d486e664b3aa2fa9842a
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2019-02-28 16:10:14 +0300

    invert function for mat4 class

--- EmreCanEngine3D/EmreCanEngine3D/src/maths/mat4.h
+++ EmreCanEngine3D/EmreCanEngine3D/src/maths/mat4.h
@@ -17,6 +17,12 @@ namespace EmreCan3D
 			mat4();
 			mat4(float diagoal);
 
+			vec4 getColumn(int index)
+			{
+				index *= 4;
+				return vec4(elements[index + 0], elements[index + 1], elements[index + 2], elements[index + 3]);
+			}
+
 			static mat4 identity();
 
 			mat4& multiply(const mat4& other);
@@ -29,8 +35,10 @@ namespace EmreCan3D
 			vec4 multiply(const vec4& other) const;
 			friend vec4 operator*(const mat4& left, const vec4& right);
 
+			mat4& invert();
+
 			static mat4 orthographic(float left, float right, float bottom, float top, float near, float far);
-			static mat4 perspective(float fow, float aspectRatio,float near, float far);
+			static mat4 perspective(float fow, float aspectRatio, float near, float far);
 
 			static mat4 translate(const vec3& translation);
 			static mat4 rotate(float angle, const vec3& axis);
